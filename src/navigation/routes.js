6b010504c3989b9c3d0eab192ceec749c5ff6395
alexandresette <alexandreclayton@gmail.com>
2018-03-27 13:12:55 -0300

    [Teste Drawer Navigator]
    - ajustando drawer naviagation

--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -1,16 +1,36 @@
+import React from 'react'
 import { StackNavigator, DrawerNavigator } from 'react-navigation'
-import { Platform, StatusBar } from 'react-native'
+import { Text } from 'react-native'
 import { isLogged } from 'services/firebase'
 import { colors, metrics } from 'styles'
 
 import Login from 'pages/login'
 import Main from 'pages/main'
 
+
+// Main DrawerStack
+const contentDrawer = DrawerNavigator({ screen: Main })
+const DrawerNavigation = StackNavigator({ DrawerStack: contentDrawer }, {
+  headerMode: 'float',
+  navigationOptions: ({ navigation }) => ({
+    title: 'Logged In to your app!',
+    headerLeft: <Text onPress={() => navigation.navigate('DrawerOpen')}>Menu</Text>,
+  }),
+})
+// Login Stack
+const LoginStack = StackNavigator({
+  SingIn: { screen: Login },
+  SingUp: { screen: Login, navigationOptions: { title: 'Nova Conta' } },
+}, { headerMode: 'float' })
+
+// Root Stack Navigation
 export default StackNavigator({
-  Login: { screen: Login },
-  Main: DrawerNavigator({ screen: Main }),
+  loginStack: { screen: LoginStack },
+  drawerStack: { screen: DrawerNavigation },
 }, {
-  initialRouteName: 'Main',
+  headerMode: 'none',
+  title: 'Main',
+  initialRouteName: 'loginStack',
   navigationOptions: () => ({
     headerStyle: {
       backgroundColor: colors.primaryColorDark,
